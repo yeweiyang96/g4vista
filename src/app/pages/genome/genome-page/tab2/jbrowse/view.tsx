6dc82fccadc2736cf695694b3a35df14b5306e68
yeweiyang96 <yeweiyang96@gmail.com>
2024-10-21 16:23:58 +0900

    feat: 添加GRCh38基因组组装和相关轨道配置

--- src/app/pages/genome/genome-page/tab2/jbrowse/view.tsx
+++ src/app/pages/genome/genome-page/tab2/jbrowse/view.tsx
@@ -4,10 +4,12 @@ import {
   createViewState,
   JBrowseLinearGenomeView,
 } from '@jbrowse/react-linear-genome-view'
+import assembly from './assembly'
+import tracks from './tracks1'
+import defaultSession from './defaultSession'
 import { onStateChange } from './jbrowse.service'
 // import { G4GTrack,G4CTrack,GeneTrack }  from './jbrowse.config'
-// import defaultSession  from './defaultSession'
-import config from './config'
+
 
 type ViewModel = ReturnType<typeof createViewState>
 interface ViewProps {
@@ -21,26 +23,26 @@ const View: React.FC<ViewProps> = ({assemblyName,locString}) => {
   const [viewState, setViewState] = useState<ViewModel>()
   // const [patches, setPatches] = useState('')
   // const [stateSnapshot, setStateSnapshot] = useState('')
-  const [text, setStateLocal] = useState(null)
+  // const [text, setStateLocal] = useState(null)
 
-  const assembly = {
-    name: assemblyName,
-    sequence: {
-      type: 'ReferenceSequenceTrack',
-      trackId: 'ReferenceSequenceTrack',
-      adapter: {
-        type: 'BgzipFastaAdapter',
-        fastaLocation: {
-          uri: url + assemblyName + '/' + assemblyName + '.fa.gz',
-        },
-        faiLocation: {
-          uri: url + assemblyName + '/' + assemblyName + '.fa.gz.fai',
-        },
-        gziLocation: {
-          uri: url + assemblyName + '/' + assemblyName + '.fa.gz.gzi',
-        },
-      },
-    }}
+  // const assembly = {
+  //   name: assemblyName,
+  //   sequence: {
+  //     type: 'ReferenceSequenceTrack',
+  //     trackId: 'ReferenceSequenceTrack',
+  //     adapter: {
+  //       type: 'BgzipFastaAdapter',
+  //       fastaLocation: {
+  //         uri: url + assemblyName + '/' + assemblyName + '.fa.gz',
+  //       },
+  //       faiLocation: {
+  //         uri: url + assemblyName + '/' + assemblyName + '.fa.gz.fai',
+  //       },
+  //       gziLocation: {
+  //         uri: url + assemblyName + '/' + assemblyName + '.fa.gz.gzi',
+  //       },
+  //     },
+  //   }}
     // track.assemblyNames = [assemblyName]
     // track.adapter.gffGzLocation = url + assemblyName + '/' + assemblyName + '_g.gff.gz'
     // track.adapter.index.location = track.adapter.gffGzLocation + '.tbi'
@@ -48,56 +50,57 @@ const View: React.FC<ViewProps> = ({assemblyName,locString}) => {
 
 
   useEffect(() => {
-    const subscription = onStateChange((newState) => {
-      setStateLocal(newState);
-    });
+    // const subscription = onStateChange((newState) => {
+    //   setStateLocal(newState);
+    // });
     const state = createViewState({
-
       assembly,
-      tracks:[
-        {
-          type: 'FeatureTrack',
-          trackId: 'g4_g',
-          name: 'G-Rich Sequences',
-          assemblyNames: [assemblyName],
-          adapter: {
-            type: 'Gff3TabixAdapter',
-            gffGzLocation: {uri: 'https://g4vista-api.med.niigata-u.ac.jp/jbrowse/aaa/aaa_g.gff.gz'},
-            index: {
-              location: {uri:'https://g4vista-api.med.niigata-u.ac.jp/jbrowse/aaa/aaa_g.gff.gz.tbi'},
-              indexType: 'TBI'
-            },
-          },
-          displays: [
-            {
-              id: 'g4g',
-              displayId: 'g4_g-LinearBasicDisplay',
-              type: 'LinearBasicDisplay',
-              configuration: 'g4_g-LinearBasicDisplay',
-              renderer: {
-                type: 'SvgFeatureRenderer',
-                color1:
-                  "jexl: cast({ T1_G_Rich: '#388E3C', T2_G_Rich: '#43A047', T3_G_Rich: '#4CAF50', T4_G_Rich: '#66BB6A'})[get(feature, 'type')]",
-                color2: 'blue',
-                color3: '#DEA3DA',
-              },
-            },
-          ],
+      tracks,
+      defaultSession,
+      // tracks:[
+      //   {
+      //     type: 'FeatureTrack',
+      //     trackId: 'g4_g',
+      //     name: 'G-Rich Sequences',
+      //     assemblyNames: [assemblyName],
+      //     adapter: {
+      //       type: 'Gff3TabixAdapter',
+      //       gffGzLocation: {uri: 'https://g4vista-api.med.niigata-u.ac.jp/jbrowse/aaa/aaa_g.gff.gz'},
+      //       index: {
+      //         location: {uri:'https://g4vista-api.med.niigata-u.ac.jp/jbrowse/aaa/aaa_g.gff.gz.tbi'},
+      //         indexType: 'TBI'
+      //       },
+      //     },
+      //     displays: [
+      //       {
+      //         id: 'g4g',
+      //         displayId: 'g4_g-LinearBasicDisplay',
+      //         type: 'LinearBasicDisplay',
+      //         configuration: 'g4_g-LinearBasicDisplay',
+      //         renderer: {
+      //           type: 'SvgFeatureRenderer',
+      //           color1:
+      //             "jexl: cast({ T1_G_Rich: '#388E3C', T2_G_Rich: '#43A047', T3_G_Rich: '#4CAF50', T4_G_Rich: '#66BB6A'})[get(feature, 'type')]",
+      //           color2: 'blue',
+      //           color3: '#DEA3DA',
+      //         },
+      //       },
+      //     ],
 
 
-        }
-      ],
+      //   }
+      // ],
       // defaultSession,
-      plugins: [],
-      location: locString,
+      // plugins: [],
+      // location: locString,
 
       // onChange: patch => {
       //   setPatches(previous => previous + JSON.stringify(patch) + '\n')
       // },
       configuration: {
-        rpc: {
-          defaultDriver: 'WebWorkerRpcDriver',
-        },
+        // rpc: {
+        //   defaultDriver: 'WebWorkerRpcDriver',
+        // },
         theme :{
           palette: {
             primary: {
@@ -117,24 +120,23 @@ const View: React.FC<ViewProps> = ({assemblyName,locString}) => {
       },
       hydrateFn: hydrateRoot,
       createRootFn: createRoot,
-      makeWorkerInstance: () => {
-        return new Worker(new URL('./rpc-worker.worker', import.meta.url), {
-          type: 'module',
-        })
-      },
+      // makeWorkerInstance: () => {
+      //   return new Worker(new URL('./rpc-worker.worker', import.meta.url), {
+      //     type: 'module',
+      //   })
+      // },
     })
-    state.session.view.showTrack('ReferenceSequenceTrack')
+    // state.session.view.showTrack('ReferenceSequenceTrack')
     // state.session.view.showTrack('g4_c')
-    state.session.view.showTrack('g4_g')
+    // state.session.view.showTrack('g4_g')
     // state.session.view.showTrack('genes')
 
     setViewState(state)
     // 清理订阅
-    return () => {
-      subscription.unsubscribe();
-    };
+    // return () => {
+    //   subscription.unsubscribe();
+    // };
   }, [])
-
 
   if (!viewState) {
     return null
